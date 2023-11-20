d7c298bbade8ab8961d2d076db2bec24283a182a
allwyn.oommen <allwyn.oommen@internetstores.com>
2023-11-20 14:38:53 +0100

    First Commit

---
+++ src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../compnents/Navbar';
+import Footer from '../compnents/Footer';
+import Card from '../compnents/Card';
+import Carousel from '../compnents/Carousel';
+
+export default function Home() {
+    return (
+        <div>
+            <div><Navbar/></div>
+            <div><Carousel/></div>
+            <div className='m-3'><Card/></div>
+            <div><Footer /></div>
+        </div>
+    )
+}
